1cdc681a009530fb43d2e13064a44490db1da9da
Abhishek Singh <abhishesingh@microsoft.com>
2023-10-30 22:30:11 +0530

    Test commit - remove custom context extra types

--- sdk/communication/Azure.Communication.CallAutomation/src/Models/CustomContext.cs
+++ sdk/communication/Azure.Communication.CallAutomation/src/Models/CustomContext.cs
@@ -12,10 +12,10 @@ namespace Azure.Communication.CallAutomation
     public class CustomContext
     {
         /// <summary> Dictionary of VOIP headers. </summary>
-        public IDictionary<string, string> VoipHeaders { get; }
+        public IDictionary<string, string> VoipHeaders { get; set; }
 
         /// <summary> Dictionary of SIP headers. </summary>
-        public IDictionary<string, string> SipHeaders { get; }
+        public IDictionary<string, string> SipHeaders { get; set; }
 
         /// <summary>
         /// Creates a new CustomContext.
@@ -24,42 +24,6 @@ namespace Azure.Communication.CallAutomation
         {
             SipHeaders = sipHeaders;
             VoipHeaders = voipHeaders;
-        }
-
-        /// <summary>
-        /// Add a custom context sip or voip header.
-        /// </summary>
-        /// <param name="header">custom context sip UUI, custom header or voip header.</param>
-        public void Add(CustomContextHeader header)
-        {
-            if (header is SIPUUIHeader sipUUIHeader)
-            {
-                if (SipHeaders == null)
-                {
-                    throw new InvalidOperationException("Cannot add sip header, SipHeaders is null.");
-                }
-                SipHeaders.Add(sipUUIHeader.Key, sipUUIHeader.Value);
-            }
-            else if (header is SIPCustomHeader sipCustomHeader)
-            {
-                if (SipHeaders == null)
-                {
-                    throw new InvalidOperationException("Cannot add sip header, SipHeaders is null.");
-                }
-                SipHeaders.Add(sipCustomHeader.Key, sipCustomHeader.Value);
-            }
-            else if (header is VoipHeader voipHeader)
-            {
-                if (VoipHeaders == null)
-                {
-                    throw new InvalidOperationException("Cannot add voip header, VoipHeaders is null");
-                }
-                VoipHeaders.Add(voipHeader.Key, voipHeader.Value);
-            }
-            else
-            {
-                throw new InvalidOperationException("Unknown custom context header type.");
-            }
         }
     }
 }
